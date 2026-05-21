d20905b07950ca7b4c68f04b5cd17f5fe650b937
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-21 15:55:49 +0200

    Settato .get al root e .listen

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-import {express} from 'express';
+import express from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 const URL = process.env.URL;
+
+app.get('/', (request, response) => {
+    response.json({
+        messaggio: 'Server del mio blog'
+        
+    });
+});
+
+
+
+
+
+app.listen(PORT, (error) => {
+    if (error){
+        console.error('Si è stato un errore: ', error);
+    } else {
+        console.log(`Server in ascolto alla porta ${PORT}`);
+    }
+    
+});
