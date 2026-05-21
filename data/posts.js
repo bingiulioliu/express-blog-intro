fa7a2b7a68ed3f7b945ed2ea63beaebaa3509b59
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-21 16:14:02 +0200

    ADD post in data

---
+++ data/posts.js
@@ -0,0 +1,34 @@
+const diarioDiBordo = [
+    {
+        titolo: "La tempesta del Capo Horn",
+        contenuto: "Onde alte come montagne hanno tentato di inghiottire la nostra cara nave. Abbiamo perso tre botti di rum, ma la scafo ha tenuto.",
+        immagine: "tempesta.webp",
+        tags: ["avventura", "mare", "pericolo"]
+    },
+    {
+        titolo: "Terra all'orizzonte!",
+        contenuto: "Dopo 40 giorni di infinito blu, abbiamo avvistato un'isola che non compare su nessuna mappa. Sembra che le palme luccichino d'oro.",
+        immagine: "isola_misteriosa.webp",
+        tags: ["esplorazione", "scoperta", "oro"]
+    },
+    {
+        titolo: "Ammutinamento sventato",
+        contenuto: "Il cuoco di bordo tramava alle mie spalle con il nostromo. Ho risolto la questione con un duello di sciabole all'alba. Ora il cuoco è il pesce.",
+        immagine: "sciabola.webp",
+        tags: ["disciplina", "pirati", "lotta"]
+    },
+    {
+        titolo: "Il tesoro di Barbanera",
+        contenuto: "Abbiamo trovato la cassa! Non erano monete, ma mappe nautiche antiche che indicano la rotta verso il kraken. Un tesoro di conoscenza.",
+        immagine: "mappa_tesoro.webp",
+        tags: ["tesoro", "leggende", "mare"]
+    },
+    {
+        titolo: "Notte sotto le stelle",
+        contenuto: "Il mare è una tavola di vetro. La ciurma canta canzoni di tempi lontani e la luna illumina la rotta verso casa. O forse verso il prossimo bottino.",
+        immagine: "notte_stellata.webp",
+        tags: ["relax", "canti", "orizzonte"]
+    }
+];
+
+export default diarioDiBordo;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import diarioDiBordo from './data/posts';
 
 const app = express();
 
